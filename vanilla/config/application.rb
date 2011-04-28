28780e4aed4beaddc8682d44c823d044c83f94c2
Ryan Greenhall <ryan.greenhall@gmail.com>
2011-04-28 14:53:08 +0100

    Specifying haml and rspec as default tools

--- vanilla/config/application.rb
+++ vanilla/config/application.rb
@@ -35,6 +35,11 @@ module Vanilla
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
+    
+    config.generators do |g|
+      g.template_engine :haml
+      g.test_framework  :rspec
+    end
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
